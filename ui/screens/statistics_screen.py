910cb852976ce3fc335e894fc0f7ef69d8c1116f
Laajf <crymov.artyom@yandex.ru>
2025-10-26 21:32:22 +0300

    Улучшен дизайн вкладки календарь

--- ui/screens/statistics_screen.py
+++ ui/screens/statistics_screen.py
@@ -1,15 +1,18 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
+from kivy.properties import NumericProperty, ListProperty
 import os
 
-# Загружаем KV-файл
 kv_path = os.path.join(os.path.dirname(__file__), '..', 'kv', 'statistics_screen.kv')
 Builder.load_file(kv_path)
 
 
 class StatisticsScreen(Screen):
     """Экран статистики продуктивности."""
+
+    completed_tasks = NumericProperty(12)
+    productivity_percent = NumericProperty(75)
+    chart_data = ListProperty([])
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
